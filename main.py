b392e3c69d8389472f57e7b447f20d41aff9882c
กันตพัฒน์ แพทย์พงศ์ <kpongpat2557@gmail.com>
2024-02-10 03:19:57 +0700

    game logic is almost complete

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ import random
 
 class Gamescreen(App):
     def build(self):
-        self.layout = self.createtable()
-        Clock.schedule_interval(self.random_letter,0.1)  # Schedule random_letter function to run every 3 seconds
+        self.layout = GridLayout(cols=1, rows=2) 
+        self.playzone = self.playtable()
+        self.layout.add_widget(self.playzone)
+        self.input = self.inputbox()
+        self.layout.add_widget(self.input)
+        Clock.schedule_interval(self.random_letter,1)  # Schedule random_letter function to run every 3 seconds
         return self.layout
 
-    def createtable(self):
-        Screen = GridLayout(cols=1, rows=2)
+    def playtable(self):
         playzone = GridLayout(cols=6, rows=6)
-        Screen.add_widget(playzone)
         # Initialize grid with labels
         self.labels = []
         for i in range(36):
@@ -25,19 +27,32 @@ class Gamescreen(App):
             self.labels.append(label)
             playzone.add_widget(label)
         
+        return playzone
+    
+    def inputbox(self):
         input_box = BoxLayout(size_hint_y=None, height=0.1*Window.height)
-        Screen.add_widget(input_box)
-        # Add a row for text input
-        self.text_input = TextInput(text='Enter text here')
-        input_box.add_widget(self.text_input)
+        input = TextInput(text='',multiline=False,on_text_validate=self.check_char)
+        input_box.add_widget(input)
         
-        return Screen
+        return input_box
 
     def random_letter(self, dt):
         # Function to place a random letter on the grid
         char = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")  # Randomly choose a letter
         label = random.choice(self.labels)  # Randomly select a label from the grid
         label.text = char  # Set the text of the label to the random letter
+
+    def check_char(self, instance) :
+        char_for_check = instance.text.upper()  # Get input text and convert to uppercase for case insensitivity
+        for label in self.labels:
+            if label.text == char_for_check:
+                label.text = ''  # Clear the label text if it matches the input text
+                break  # Break the loop after clearing one matching letter
+        instance.text = ''  # Clear the input box after checking
+        Clock.schedule_once(lambda dt: self.focus_input(instance))
+
+    def focus_input(self, instance):
+        instance.focus = True  # Set focus back to the input box
 
 if __name__ == "__main__":
     Gamescreen().run()
